193e9c0efb41a7a0cec12d20cc3ce588b77c72b6
Antonis Karatantsis <karatantsis@gmail.com>
2022-11-09 07:53:35 +0000

    Added yargs.parse() function at the end of the script, so yargs does it's thing

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const yargs = require("yargs");
 
 // Process args the right way
 
+//******************************************************
 // Create add command
 yargs.command({
   command: "add",
@@ -19,12 +20,13 @@ yargs.command({
       type: "string",
     },
   },
-  handler: (argv) => {
-    console.log(`Title: ${argv.title}`);
-    console.log(`Body: ${argv.body}`);
+  handler: ({ title, body }) => {
+    console.log(`Title: ${title}`);
+    console.log(`Body: ${body}`);
   },
 });
 
+//******************************************************
 // Create delete command
 yargs.command({
   command: "remove",
@@ -36,20 +38,22 @@ yargs.command({
       type: "string",
     },
   },
-  handler: (argv) => {
-    console.log(`Removing note titled "${argv.title}"`);
+  handler: ({ title }) => {
+    console.log(`Removing note titled "${title}"`);
   },
 });
 
+//******************************************************
 // Create list command
 yargs.command({
   command: "list",
   describe: "List all notes",
-  handler: (argv) => {
+  handler: () => {
     console.log(`Listing out all notes`);
   },
 });
 
+//******************************************************
 // Create read command
 yargs.command({
   command: "read",
@@ -61,9 +65,9 @@ yargs.command({
       type: "string",
     },
   },
-  handler: (argv) => {
-    console.log(`Reading note titled "${argv.title}"`);
+  handler: ({ title }) => {
+    console.log(`Reading note titled "${title}"`);
   },
 });
 
-yargs.argv;
+yargs.parse();
